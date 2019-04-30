72c5702482d9d06afdce886d8f54952701cbddca
ParamPatel207 <ppparampatel@icloud.com>
2019-04-29 17:33:52 -0700

    first commit

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.use("/restaurants/:id/comments", commentRoute);
 app.use("/users", userRoute);
 app.use("/", passwordRoute);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, process.env.IP, function(){
   console.log("The Restaurant Server Has Started!");
 });
